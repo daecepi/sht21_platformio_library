cc5c19f91fac792c36ed37c111c2beb947af23ea
daecepi <daecepi@gmail.com>
2017-03-06 00:27:13 -0500

    Adaptación del codigo

--- src/sht21Library.cpp
+++ src/sht21Library.cpp
@@ -1,55 +1,115 @@
-#include <inttypes.h>
-#include <Wire.h>
+#if defined(ARDUINO)
+#include "Wire.h"
 #include "sht21Library.h"
-#include <Arduino.h>
+#endif
 
-float sht21LibraryClass::getHumidity(void)
+//Inicialización
+sht21Library::sht21Library(){}
+
+void sht21Library::iniciar()
 {
-  uint16_t result;
+  Wire.begin();
 
-  //Comenzar comunicación
-  Wire.beginTransmission(eSHT2xAddress);	//Comenzar transmision de datos
-  Wire.write(eRHumidityHoldCmd);					//Enviar puntero
-  delay(100);
-  Wire.endTransmission();               	//Finalizar transmision
+  uint8_t ID_Temp_Hum = checkID();
 
-  Wire.requestFrom(eSHT2xAddress, 3); //Petición de datos al sensor
-  //Espera de datos humedad
-  while(Wire.available() < 3) {
-    ;
+  int x = 0;
+
+  if(ID_Temp_Hum == 0x15)//Ping CheckID register
+    x = 1;
+  else if(ID_Temp_Hum == 0x32)
+    x = 2;
+  else
+    x = 0;
+
+  if(x == 1)
+  {
+    Serial.println("Si7021 encontrado");
+    //Serial.println(ID_Temp_Hum, HEX);
   }
-
-  //Guardar resultados
-  result = ((Wire.read()) << 8);
-  result += Wire.read();
-	result &= ~0x0003;   // Limpiar los bits de status
-
-	return (-6.0 + 125.0 / 65536.0 * ((float)(result)));
+  else if(x == 2)
+  {
+    Serial.println("HTU21D encontrado");
+    //Serial.println(ID_Temp_Hum, HEX);
+  }
+  else
+  	Serial.println("Ningun dispositivo detectado");
+  	//Serial.println(ID_Temp_Hum, HEX);
 }
 
-float sht21LibraryClass::getTemperature(void)
+float sht21Library::getHumedad()
 {
-  uint16_t result;
-
-  //Comenzar comunicación
-  Wire.beginTransmission(eSHT2xAddress);	//Comenzar transmision de datos
-  Wire.write(eTempHoldCmd);					//send the pointer location
-  delay(100);
-  Wire.endTransmission();               	//end
-
-  //Espera de datos de humedad
-  Wire.requestFrom(eSHT2xAddress, 3); //Petición de datos al sensor
-  //Espera de datos de temperatura
-  while(Wire.available() < 3) {
-    ;
-  }
-
-  //Guardar resultados
-  result = ((Wire.read()) << 8);
-  result += Wire.read();
-	result &= ~0x0003;   // Limpiar los bits de status
-
-  return (-46.85 + 175.72 / 65536.0 * ((float)(result)));
+	// Measure the relative humidity
+	uint16_t RH_Code = tomarDatos(HUMD_MEASURE_NOHOLD);
+	float resultado = (125.0*RH_Code/65536)-6;
+	return resultado;
 }
 
-sht21LibraryClass SHT21;
+float sht21Library::getTemperaturaDeHumedad()
+{
+	// Medir temperatura de la anterior medición de humedad.
+	uint16_t temp_Code = tomarDatos(TEMP_PREV);
+	float resultado = (175.25*temp_Code/65536)-46.85;
+	return resultado;
+}
+
+float sht21Library::getTemperatura()
+{
+	// Medir temperatura
+	uint16_t temp_Code = tomarDatos(TEMP_MEASURE_NOHOLD);
+	float resultado = (175.25*temp_Code/65536)-46.85;
+	return resultado;
+}
+
+uint8_t sht21Library::checkID()
+{
+	uint8_t ID_1;
+
+ 	// Check device ID
+	Wire.beginTransmission(ADDRESS);
+	Wire.write(0xFC);
+	Wire.write(0xC9);
+	Wire.endTransmission();
+
+    Wire.requestFrom(ADDRESS,1);
+
+    ID_1 = Wire.read();
+
+    return(ID_1);
+}
+
+uint16_t sht21Library::tomarDatos(uint8_t command)
+{
+
+	uint16_t nBytes = 3;
+	// if we are only reading old temperature, read olny msb and lsb
+	if (command == 0xE0) nBytes = 2;
+
+	Wire.beginTransmission(ADDRESS);
+	Wire.write(command);
+	Wire.endTransmission();
+	// When not using clock stretching (*_NOHOLD commands) delay here
+	// is needed to wait for the measurement.
+	// According to datasheet the max. conversion time is ~22ms
+	 delay(100);
+
+	Wire.requestFrom(ADDRESS,nBytes);
+	//Wait for data
+	int counter = 0;
+	while (Wire.available() < nBytes){
+	  delay(1);
+	  counter ++;
+	  if (counter >100){
+	    // Timeout: Sensor did not return any data
+	    return 100;
+	  }
+	}
+
+	unsigned int msb = Wire.read();
+	unsigned int lsb = Wire.read();
+	// Clear the last to bits of LSB to 00.
+	// According to datasheet LSB of RH is always xxxxxx10
+	lsb &= 0xFC;
+	unsigned int datos = msb << 8 | lsb;
+
+	return datos;
+}
--- src/sht21Library.h
+++ src/sht21Library.h
@@ -1,26 +1,47 @@
 #ifndef sht21Library_h
 #define sht21Library_h
 
-#include <inttypes.h>
+#include <Arduino.h>
 
-typedef enum {
-    eSHT2xAddress = 0x40,
-} HUM_SENSOR_T;
+/****************Si7021 & HTU21D Definitions***************************/
 
-typedef enum {
-    eTempHoldCmd		= 0xE3,
-    eRHumidityHoldCmd	= 0xE5,
-    eTempNoHoldCmd      = 0xF3,
-    eRHumidityNoHoldCmd = 0xF5,
-} HUM_MEASUREMENT_CMD_T;
+#define ADDRESS      0x40
 
-class sht21LibraryClass
+#define TEMP_MEASURE_HOLD  0xE3
+#define HUMD_MEASURE_HOLD  0xE5
+#define TEMP_MEASURE_NOHOLD  0xF3
+#define HUMD_MEASURE_NOHOLD  0xF5
+#define TEMP_PREV   0xE0
+
+#define WRITE_USER_REG  0xE6
+#define READ_USER_REG  0xE7
+#define SOFT_RESET  0xFE
+
+#define HTRE        0x02
+#define _BV(bit) (1 << (bit))
+
+#define CRC_POLY 0x988000 // Shifted Polynomial for CRC check
+
+// Error codes
+#define I2C_TIMEOUT 	998
+#define BAD_CRC		999
+
+
+class sht21Library
 {
   public:
-    float getHumidity(void);
-    float getTemperature(void);
+    sht21Library(void);
+
+    void iniciar(void);
+
+    float getHumedad(void);
+    float getTemperaturaDeHumedad(void);
+    float getTemperatura(void);
+    uint8_t  checkID(void);
+
+  private:
+    uint16_t tomarDatos(uint8_t command);
 };
 
-extern sht21LibraryClass SHT21;
 
 #endif
